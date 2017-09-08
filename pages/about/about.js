daadf83a09ff26571beb1d92c47939c8926b8bf0
squirrelRao <hqraop@163.com>
2017-09-08 23:49:07 +0800

    增加关于阳光页分享

--- pages/about/about.js
+++ pages/about/about.js
@@ -10,5 +10,24 @@ Page({
 
   onLoad: function () {
   
+  },
+  //分享
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '关于LEAD阳光',
+      path: '/pages/about/about',
+      success: function (res) {
+        // 转发成功
+        console.log(res);
+      },
+      fail: function (res) {
+        // 转发失败
+        console.log(res);
+      }
+    }
   }
 })
