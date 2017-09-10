a770ee9bc1f26195c0838d499743810916768fee
王轶驰 <wangyichi@futurelab.tv>
2017-09-10 09:12:58 +0800

    修改登录

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,5 +1,5 @@
 //logs.js
-
+var app = getApp();
 var img ="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1503247087783&di=e2423e4f765bf38b3d4040862f064e65&imgtype=0&src=http%3A%2F%2Fp4.qqgexing.com%2Ftouxiang%2F20120810%2F1502%2F5024b21b74511.jpg";
 
 Page({
@@ -10,7 +10,7 @@ Page({
     user_avatar:img
   },
   onLoad: function () {
-    var user = getApp().data.user;
+    var user = app.data.user;
     console.log(user);
     if(user){
       this.setData({
@@ -20,23 +20,73 @@ Page({
       });
     }else {
      //弹出登录
+
+      wx.getSetting({
+        success(res) {
+          console.log(res);
+          if (!res.authSetting['scope.record']) {
+            wx.authorize({
+              scope: 'scope.userInfo',
+              success(res) {
+                console.log(res);
+
+              },
+              complete:function(res){
+                console.log(res);
+              }
+            })
+          }
+        }
+      })
+    }
+  },
+  loginIn(){
+    console.log(1);
+    var that = this;
+    if (!this.data.login){
       wx.login({
         success: function (res) {
           console.log(res);
           if (res.code) {
             //发起网络请求
-            // wx.request({
-            //   url: 'https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code',
-            //   data: {
-            //     code: res.code
-            //   }
-            // })
+            wx.getUserInfo({
+              success: function (res) {
+                console.log(res);
+                that.data.user = res.userInfo
+              },
+              fail: function (res) {
+                console.log(res);
+              }
+            })
           } else {
             console.log('获取用户登录态失败！' + res.errMsg)
           }
         }
       });
+      wx.getSetting({
+        success(res) {
+          console.log(res);
+          if (!res.authSetting['scope.record']) {
+            wx.authorize({
+              scope: 'scope.userInfo',
+              success(res) {
+                console.log(res);
+              },
+              complete: function (res) {
+                console.log(res);
+              }
+            })
+          }
+        }
+      })
     }
+  },
+  loginOut(){
+    this.setData({
+      user_nickname: "未登录",
+      user_avatar: img,
+      login: false,
+    })
   },
   toPage:function(e){
     console.log(parseInt(e.target.dataset.id));
