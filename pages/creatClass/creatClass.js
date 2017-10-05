569784096a828a47b3316bc1066f3986d77e1863
王轶驰 <wangyichi@futurelab.tv>
2017-10-05 12:59:23 +0800

    修改选择器

--- pages/creatClass/creatClass.js
+++ pages/creatClass/creatClass.js
@@ -5,8 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    schoolArray: ["定福小学","朱房村社区","经纬学校"],
+    classArray: ["数学", "英语", "围棋","化学","足球","计算机","绘本","舞蹈"],
+    lessionArray: ["2015秋季", "2016春季", "2016秋季", "2017春季", "2017秋季", "2018春季", "2018秋季", "2019春季"],
+    frequencyArray:["第一次","第二次","第三次","第四次","第五次"],
+    schoolIndex: 0,
+    classIndex:0,
+    lessionIndex:0,
+    frequencyIndex:0
   },
+  bindPickerSchoolChange: function (e) {
+    this.setData({
+      schoolIndex: e.detail.value
+    })
+  },
+    bindPickerClassChange: function (e) {
+      this.setData({
+        classIndex: e.detail.value
+      })
+  },
+    bindPickerLessionChange: function (e) {
+      this.setData({
+        lessionIndex: e.detail.value
+      })
+    },
+    bindPickerFrequencyChange:function(e){
+      this.setData({
+        frequencyIndex: e.detail.value
+      })
+    },
 
   /**
    * 生命周期函数--监听页面加载
